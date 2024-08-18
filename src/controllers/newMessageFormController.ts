d9c681c73b229f708cfd8f0757270795cdb003bc
yppelu <yppelu@gmail.com>
2024-08-18 12:24:07 +0700

    Set up functionality for adding a message

--- src/controllers/newMessageFormController.ts
+++ src/controllers/newMessageFormController.ts
@@ -1,11 +1,13 @@
 import { Request, Response } from 'express-serve-static-core';
+import { addMessage } from '../model/messages/messages';
 
 const newMessageFormController = {
   get(req: Request, res: Response) {
     res.render('./pages/newMessageForm.ejs', { title: 'Add New Message' });
   },
   post(req: Request, res: Response) {
-    res.end();
+    addMessage(req.body.user, req.body.text);
+    res.redirect('/');
   }
 };
 
--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,7 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
 
 app.use('/', indexRouter);
 app.use('/new', newMessageFormRouter);
